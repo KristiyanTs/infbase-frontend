7b803ca9bef5dd960b7eb31d3a6c1d307de6afff
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-02-15 20:38:57 +0000

    require login before viewing some pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,83 +1,89 @@
 <template>
-  <section class="section section-shaped section-lg my-0">
-    <div class="shape shape-style-1 bg-gradient-default">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    <div class="container pt-lg-md">
-      <div class="row justify-content-center">
-        <div class="col-lg-5">
-          <card
-            type="secondary"
-            shadow
-            header-classes="bg-white pb-5"
-            body-classes="px-lg-5 py-lg-5"
-            class="border-0"
-          >
-            <div class="text-center text-muted mb-3">
-              <small>Sign in</small>
-            </div>
-            <base-alert type="danger" v-if="failure">
-              {{response}}
-            </base-alert>
-            <form id="loginForm" role="form">
-              <base-input
-                alternative
-                required
-                class="mb-3"
-                placeholder="Email"
-                addon-left-icon="ni ni-email-83"
-                v-model="email"
-              >
-              </base-input>
-              <base-input
-                alternative
-                input_type="password"
-                placeholder="Password"
-                addon-left-icon="ni ni-lock-circle-open"
-                v-model="password"
-              >
-              </base-input>
-              <base-checkbox v-model="remember_me">
-                Remember me
-              </base-checkbox>
-              <div class="text-center">
-                <base-button
-                  type="primary"
-                  class="my-4"
-                  v-on:click="submit()"
-                >
-                  Sign In
-                </base-button>
-              </div>
-            </form>
-          </card>
-          <div class="row mt-3">
-            <div class="col-6">
-              <router-link to="/password" class="text-light">
-                <small>Forgot password?</small>
-              </router-link>
-            </div>
-            <div class="col-6 text-right">
-              <router-link to="/signup" class="text-light">
-                <small>Create new account</small>
-              </router-link>
-            </div>
-          </div>
+    <section class="section section-shaped section-lg my-0">
+        <div class="shape shape-style-1 bg-gradient-default">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
-      </div>
-    </div>
-  </section>
+        <div class="container pt-lg-md">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <card
+                            type="secondary"
+                            shadow
+                            header-classes="bg-white pb-5"
+                            body-classes="px-lg-5 py-lg-5"
+                            class="border-0"
+                    >
+                        <div class="text-center text-muted mb-3">
+                            <small>Sign in</small>
+                        </div>
+                        <base-alert type="danger" v-if="failure">
+                            {{response}}
+                        </base-alert>
+                        <form id="loginForm" role="form">
+                            <base-input
+                                    alternative
+                                    required
+                                    class="mb-3"
+                                    placeholder="Email"
+                                    addon-left-icon="ni ni-email-83"
+                                    v-model="email"
+                            >
+                            </base-input>
+                            <base-input
+                                    alternative
+                                    input_type="password"
+                                    placeholder="Password"
+                                    addon-left-icon="ni ni-lock-circle-open"
+                                    v-model="password"
+                            >
+                            </base-input>
+                            <base-checkbox v-model="remember_me">
+                                Remember me
+                            </base-checkbox>
+                            <div class="text-center">
+                                <base-button
+                                        type="primary"
+                                        class="my-4"
+                                        v-on:click="submit()"
+                                >
+                                    Sign In
+                                </base-button>
+                            </div>
+                        </form>
+                    </card>
+                    <div class="row mt-3">
+                        <div class="col-6">
+                            <router-link to="/password" class="text-light">
+                                <small>Forgot password?</small>
+                            </router-link>
+                        </div>
+                        <div class="col-6 text-right">
+                            <router-link to="/signup" class="text-light">
+                                <small>Create new account</small>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
   export default {
+    props: {
+      nextUrl: {
+        type: String,
+        default: "/"
+      }
+    },
     data() {
       return {
         email: "",
@@ -88,7 +94,7 @@
       };
     },
     methods: {
-      submit: function() {
+      submit: function () {
         this.failure = false;
 
         this.axios
@@ -104,16 +110,16 @@
           })
           .catch(error => this.loginFailed(error))
       },
-      loginSuccessful (response) {
+      loginSuccessful(response) {
         this.$store.dispatch("signedIn", [
           response.headers.authorization,
           response.data.id,
           response.data.role
         ]);
         this.$store.commit("ADD_ALERT", ["You are now logged in.", "success"]);
-        this.$router.push('/');
+        this.$router.push(this.nextUrl)
       },
-      loginFailed (error) {
+      loginFailed(error) {
         this.failure = true;
         this.response = "Your email or password is incorrect";
       }
